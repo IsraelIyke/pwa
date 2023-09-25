1ed7002f6666441a5a91a7211c661cf91ddd2440
IsraelIyke <nwangwuisrael@gmail.com>
2023-09-25 23:41:56 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
-
-// module.exports = nextConfig
-
 // next.config.js
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    scope: "/",
-    sw: "service-worker.js", // You can customize the service worker filename
-  },
-  // Other Next.js configurations go here
-});
+module.exports = (phase, { defaultConfig }) => {
+  const pwaConfig = {
+    pwa: {
+      dest: "public",
+      register: true,
+      scope: "/",
+      sw: "service-worker.js", // You can customize the service worker filename
+    },
+  };
+
+  // Merge your PWA configuration with the default configuration
+  const mergedConfig = {
+    ...defaultConfig,
+    ...pwaConfig,
+    // Add other Next.js configurations here
+  };
+
+  return withPWA(mergedConfig);
+};
